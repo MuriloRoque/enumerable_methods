6ddaf99e9e788aac5f5728926b5c6b769504508b
Murilo Roque <muriloengqui@gmail.com>
2020-04-04 10:35:27 -0300

    Done my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -1,4 +1,7 @@
+# rubocop: disable Metrics/ModuleLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
 module Enumerable
+
   def my_each
     n = 0
     while n <= size-1
@@ -6,9 +9,26 @@ module Enumerable
       n += 1
     end
   end
+
+  def my_each_with_index
+    n = 0
+    while n <= size - 1
+      yield(to_a[n], n)
+      n += 1
+    end
+  end
+
 end
 
+# rubocop: enable Metrics/ModuleLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
 arr = [1,3,4,8,2]
+
 arr.my_each do |n|
-  n*2
+  print n*2
+end
+
+arr.my_each_with_index do |n, m|
+  print " #{m}:"
+  print n*2
 end
