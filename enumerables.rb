0d242e9775035c963a3b9efa5b0ea995a3841495
Murilo Roque <muriloengqui@gmail.com>
2020-04-07 05:29:40 -0300

    Finished my_any and my_none

--- enumerables.rb
+++ enumerables.rb
@@ -5,15 +5,15 @@ module Enumerable
   def my_each
     n = 0
     while n <= size-1
-      yield (to_a[n])
+      yield(self[n])
       n += 1
     end
   end
 
   def my_each_with_index
     n = 0
-    while n <= size - 1
-      yield(to_a[n], n)
+    while n <= size-1
+      yield(self[n], n)
       n += 1
     end
   end
@@ -22,11 +22,33 @@ module Enumerable
     result=[]
     my_each do |n|
       result.push(n) if yield(n) 
-      n += 1
     end
-    return result
+    result
   end
 
+  def my_all?
+    result=true
+    my_each do |n|
+      result=false unless yield(n)
+    end
+    result
+  end
+
+  def my_any?
+    result=false
+    my_each do |n|
+      return result=true if yield(n)
+    end
+    result
+  end
+
+  def my_none?
+    result=true
+    my_each do |n|
+      return result=false if yield(n)
+    end
+    result
+  end
 end
 
 # rubocop: enable Metrics/ModuleLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
@@ -35,13 +57,27 @@ arr = [1,3,4,8,2]
 
 arr.my_each do |n|
   print n*2
+  print " "
 end
+
+print "\n"
 
 arr.my_each_with_index do |n, m|
-  print " #{m}:"
-  print n*2
+  print "#{m}:#{n*2} / "
 end
 
-arr.my_select do |n|
-  n%2==0
-end
+print "\n"
+
+puts [1,2,3,4,5].my_select { |num|  num.even?  }
+
+print "\n"
+
+puts ["ant", "bear", "cat"].my_all? { |word| word.length >= 4 }
+
+print "\n"
+
+puts ["ant", "bear", "cat"].my_any? { |word| word.length >=4 }
+
+print "\n"
+
+puts ["ant", "bear", "cat"].my_none? { |word| word.length >=4 }
